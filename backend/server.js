6c2a0374de3fdcfb988ed75e2f0805cc87ba36de
JadenJansz <jadenjansz@gmail.com>
2024-03-16 10:07:11 +0530

    add backend folder structure

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get('/api/data')
+app.post('/api/data');
+
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 })
