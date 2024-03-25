eccd50f5ea73f7e907ab4e98db2be152ad44f42d
JadenJansz <jadenjansz@gmail.com>
2024-03-25 08:45:58 +0530

    configure cors url

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
-    origin: 'http://localhost:5173/',
+    origin: 'http://localhost:5173',
     methods: ['GET','POST']
 }
 
