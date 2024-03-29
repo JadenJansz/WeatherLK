8824447d0bae042098cf6249ac0fdabaefdd1ee5
JadenJansz <jadenjansz@gmail.com>
2024-03-29 14:05:28 +0530

    add current weather coomponent

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,19 @@
 import WeatherMap from "./components/WeatherMap"
 import Header from "./components/Header"
 import Metrics from "./components/Metrics"
+import CurrentWeather from "./components/CurrentWeather";
 
 function App() {
 
   return (
     <>
       <Header />
-      <div className="flex justify-between mx-40 h-screen mb-20">
+      <div className="flex justify-between mx-28 h-full mt-10 rounded-xl mb-32 bg-white shadow-2xl">
         <div className="px-12">
           <Metrics />
         </div>
-        <div className="basis-1/3 mt-1 scale-75">
+        <div className="basis-1/2 scale-90 flex-col">
+          <CurrentWeather />
           <WeatherMap />
         </div>
       </div>
